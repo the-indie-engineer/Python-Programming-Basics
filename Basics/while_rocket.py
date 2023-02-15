026349f23950c35f0686e071f755f759aeac12a9
Thiruvarulselvan Karunanithi <109530150+the-indie-engineer@users.noreply.github.com>
2023-02-15 18:15:32 +0530

    Add files via upload

---
+++ Basics/while_rocket.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello earthians, ready for the launch?")
+num=1
+c=1
+while(c==1):
+    print("Rocket in launchpad ", num, "is launching shorlty!")
+    c=int(input("Go for launch?"))
+    num+=1
+print("That is it for today")
+ 
