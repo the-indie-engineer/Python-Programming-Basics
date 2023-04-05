a27bcdcb6273915dd43b39bb7d4469d7ad1073d1
Thiruvarulselvan Karunanithi <109530150+the-indie-engineer@users.noreply.github.com>
2023-04-05 07:05:13 +0530

    Update Sudoku_Keying.py

--- Applications/Sudoku_Keying.py
+++ Applications/Sudoku_Keying.py
@@ -17,3 +17,33 @@ def sudoku_key(n):
         for j in range(n):
             print(key[i][j], end=" ")
         print()
+    
+    count=1
+    num=n*n
+    
+    i=n//2
+    j=n-1
+    
+    while(count<=num):
+        if(i<0 and j==n):
+            i=0
+            j=n-2
+        else:
+            if(i<0):
+                i=n-1
+            if(j==n):
+                j=0
+        if(key[i][j]!=0):
+            i=i+1
+            j=j-2
+        else:
+            key[i][j]=count
+            count+=1
+            i=i-1
+            j=j+1
+            
+    print("Your key is")
+    for i in range(n):
+        for j in range(n):
+            print(key[i][j], end=" ")
+        print()
